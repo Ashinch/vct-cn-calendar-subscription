22c79cb2dbb4d43a33ac195fef0a8cb5c197c79b
Ash <Glaxyinfinite@outlook.com>
2024-03-18 11:47:24 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# VCT CN Calendar Subscription
+
+> Credit: [LOL_Game_Subscription](https://github.com/TankNee/LOL_Game_Subscription)
+
+## Subscribe
+
+Choose a link to add to your Apple Calendar or Google Calendar, etc.
+
+- **CDN:** `https://cdn.jsdelivr.net/gh/Ashinch/vct-cn-calendar-subscription/vct-cn-alarm.ics`
+- **GitHub:** `https://github.com/Ashinch/vct-cn-calendar-subscription/blob/main/vct-cn-alarm.ics`
+
+
+### Apple Calendar
+
+- **macOS:** Open Calendar.app -> File -> New Calendar Subscription... -> Input Calendar URL -> Confirm
+- **iOS:** Open Calendar app -> bottom "Calendar" button -> Add Calendar -> Add Calendar Subscription -> Input Calendar URL -> Confirm
+
+### Google Calendar
+
+_WIP_
---
+++ index.js
@@ -0,0 +1,70 @@
+const request = require("superagent");
+const icsTool = require("ics");
+const fs = require("fs");
+const API_URL = "https://val.native.game.qq.com/esports/v1/data/VAL_Match_1000015.json";
+
+
+/**
+ *
+ * @param {{}[]} games
+ */
+function packageGames(games, hasAlarm, calName) {
+    return games.map((game) => {
+        const gameDate = new Date(game.matchDate);
+        const durationMinutes = 16;
+        const gameEndDate = new Date(gameDate.getTime() + durationMinutes * 60 * 1000);
+        let gameName = "";
+        // gameName = game.secondLevelGameName;
+        // gameName += ` ${game.bMatchName}`
+        gameName += ` ${game.teamA?.teamSpName || "TBD"} vs ${game.teamB?.teamSpName || "TBD"}`
+        const hasResult = parseInt(game.scoreA) || parseInt(game.scoreB);
+        if (hasResult) {
+            gameName += ` - ${game.scoreA} : ${game.scoreB}`;
+        }
+        return {
+            title: gameName,
+            description: `${game.secondLevelGameName} ${game.bMatchName} ${game.matchFormat}`,
+            start: [gameDate.getFullYear(), gameDate.getMonth() + 1, gameDate.getDate(), gameDate.getHours(), gameDate.getMinutes()],
+            end: [gameEndDate.getFullYear(), gameEndDate.getMonth() + 1, gameEndDate.getDate(), gameEndDate.getHours(), gameEndDate.getMinutes()],
+            organizer: {
+                name: `无畏契约${game.secondLevelGameName}`,
+                email: "vct@qq.com",
+            },
+            url: "https://vct.qq.com",
+            status: "TENTATIVE",
+            calName: calName ? calName : "VCT-CN",
+            geo: { lat: 30.0095, lon: 120.2669 },
+            startInputType: "utc",
+            startOutputType: "utc",
+            endInputType: "utc",
+            endOutputType: "utc",
+            alarms: hasAlarm && !hasResult ? [{ action: "audio", trigger: { minutes: 30, before: true, repeat: 1, attachType: "VALUE=URI", attach: "Glass" } }] : [],
+        };
+    });
+}
+
+/**
+ *
+ * @param {{}} gameBundle
+ * @param {boolean} hasAlarm
+ * @param {string} gameName
+ */
+function generateICS(gameBundle, hasAlarm) {
+    const gameInfo = gameBundle.msg;
+
+    const games = packageGames(gameInfo, hasAlarm);
+    const result = icsTool.createEvents(games);
+
+    if (result.error) {
+        console.error(result.error);
+    } else {
+        fs.writeFileSync(`./vct-cn${hasAlarm ? "-alarm" : ""}.ics`, result.value);
+    }
+}
+
+async function main() {
+    const buffer = (await request.get(API_URL)).body;
+    generateICS(buffer, true);
+}
+
+main();
